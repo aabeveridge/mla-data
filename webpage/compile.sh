680222311a623c8af6a896c68e954d820d4e08a1
Aaron Beveridge <aaronbev79@gmail.com>
2015-05-22 17:32:33 -0400

    updates to webpage

--- webpage/compile.sh
+++ webpage/compile.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-pandoc report.md -s -c pandoc.css -o ~/mla_data/index.html
+pandoc report.md -s -c ~/mla_data/pandoc.css -o ~/mla_data/index.html
 
 #git add --all
 #git commit -m "newest update"
