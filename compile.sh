dc02b4b9c62707422466fe41fba5cffb6464a313
Aaron Beveridge <aaronbev79@gmail.com>
2015-06-14 17:43:02 -0400

    updates

--- compile.sh
+++ compile.sh
@@ -1,15 +1,11 @@
 #!/bin/bash
 
 #Use pandoc to create html page from markdown report.md
-pandoc report.md -s -c pandoc.css -o index.html
+pandoc report.md -s -c pandoc.css -o mla_data.html
+
+cp mla_data.html ~/aaronbev79.github.io
 
 #Commit changes and push them to github
 #git add --all
 #git commit -m "newest update"
 #git push
-
-#Update the gh-pages branch to match the master
-#git checkout gh-pages
-#git rebase master
-#git push origin gh-pages
-#git checkout master
